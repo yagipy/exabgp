1982b0af40882cd4b7478d244ae275fc9d4cd3a3
Hiroyuki Yagihashi <email@yagipy.me>
2025-03-08 17:12:18 +0900

    Delete null terminated string (#6)

--- src/exabgp/bgp/message/update/nlri/bgpls/tlvs/opaquemetadata.py
+++ src/exabgp/bgp/message/update/nlri/bgpls/tlvs/opaquemetadata.py
@@ -6,15 +6,15 @@ class OpaqueMetadata(object):
         self.opaque_type = int(opaque_type)
         self.flags = int(flags)
         self.value = str(value).encode('utf-8')
-        self.length = len(pack('!HB', self.opaque_type, self.flags) + self.value + b'\x00')
+        self.length = len(pack('!HB', self.opaque_type, self.flags) + self.value)
 
     def pack(self):
-        return pack('!HHHB', self.type, self.length, self.opaque_type, self.flags) + self.value + b'\x00'
+        return pack('!HHHB', self.type, self.length, self.opaque_type, self.flags) + self.value
 
     @staticmethod
     def unpack(data):
         opaque_type, flags = unpack('!HB', data[:3])
-        value = data[3:].rstrip(b'\x00').decode('utf-8')
+        value = data[3:].decode('utf-8')
         return OpaqueMetadata(opaque_type, flags, value)
 
     def json(self, compact=None):
